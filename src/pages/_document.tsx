85b8a06310352c007e6faab024b99069a3cd019a
Rafaela Keller <rafaela.keller.rk@gmail.com>
2024-10-23 00:03:03 -0300

    Codigo acessivel

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ import Document,{Head,Html,Main,NextScript} from "next/document"
 export default class CustomDocument extends Document {
     render(){
         return(
-            <Html>
+            <Html lang="pt-Br">
                 <Head>
                     <link rel="preconnect" href="https://fonts.googleapis.com"/>
                     <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin=""/>
--- src/utils/axeAcessibilityReporter.ts
+++ src/utils/axeAcessibilityReporter.ts
@@ -1,5 +1,5 @@
 export async function axeAcessibilityReporter(){
-    if(process.env.NODE_ENV === 'development'){
+    if(process.env.NODE_ENV === 'development' && ){
         const axe= await require('@axe-core/react')
         const React = require('react');
         const ReactDOM = require('react-dom');
